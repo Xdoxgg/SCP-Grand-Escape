9abc37d41575f2daed35357642b1b16481fe30af
Xdoxgg <145697777+Xdoxgg@users.noreply.github.com>
2024-08-27 14:28:18 +0300

    add movemest and start work with player stats

--- My project/Assets/Scripts/MovingScripts/MovingControl.cs
+++ My project/Assets/Scripts/MovingScripts/MovingControl.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class MovingControl : MonoBehaviour
 {
+    public float moveSpeed = 5.0f;
+
+
+    private CharacterController _controller;
+
     
     void Start()
     {
-        
+        _controller = GetComponent<CharacterController>();//если я правильно помю это создание кортежа из параметров контроля
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
     {
-        
+        float horisontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+
+        Vector3 moveDeraction = transform.forward * verticalInput + transform.right * horisontalInput;
+
+
+        moveDeraction.y -=9.81f*Time.deltaTime;
+
+        _controller.Move(moveDeraction*moveSpeed*Time.deltaTime);
     }
 }
---
+++ My project/Assets/Scripts/PlayerStats/Playerstats.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerstats : MonoBehaviour
+{
+    [SerializeField]
+    private float _healthState;
+    [SerializeField]
+    private float _staminaState;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
