188930570edbc18357892cc541c3768156d7ddd2
Xdoxgg <145697777+Xdoxgg@users.noreply.github.com>
2024-10-30 17:49:11 +0300

    fix a lot of bugs, uppdate interface, add interaction, uppdate player prefab, add door prefab

---
+++ SCP Grand Escape/Assets/Player/Scripts/Intatect/PlayerInteraction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public Camera mainCamera;
+    private float distance = 5f;
+    public GameObject interactionUI;
+    public TextMeshProUGUI interactionText;
+
+    private void Update()
+    {
+        InteractionRay();
+    }
+    
+    public void InteractionRay()
+    {
+        Ray ray = mainCamera.ViewportPointToRay(Vector3.one / 2f);
+        RaycastHit hit;
+
+        bool hitSomething = false;
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            IInteracteble interacteble = hit.collider.GetComponent<IInteracteble>();
+            if (interacteble != null)
+            {
+                hitSomething = true;
+                interactionText.text = interacteble.GetDescription();
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    interacteble.Interact();
+                }
+            }
+        }
+        interactionUI.SetActive(hitSomething);
+    }
+}
