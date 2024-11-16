ac6c274876323cca14f9dd7cabe024bbc01132ec
Xdoxgg <145697777+Xdoxgg@users.noreply.github.com>
2024-11-17 00:22:52 +0300

    test commit

--- Scp-Grand-Escape/Assets/Locations/ActiveObjects/DoorController.cs
+++ Scp-Grand-Escape/Assets/Locations/ActiveObjects/DoorController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour, IInteracteble
 {
-    [SerializeField] private GameObject _door;
+    [SerializeField] private GameObject _door1;
+    [SerializeField] private GameObject _door2;
     [SerializeField] private bool _doorOpen;
+    private bool _startOpen;
     
     public bool IsInventereble()
     {
@@ -20,8 +22,26 @@ public class DoorController : MonoBehaviour, IInteracteble
 
     public void Interact()
     {
-        _door.SetActive(_doorOpen);
-        _doorOpen = !_doorOpen;
+        // while (_door1.transform.position.x<-2.97f+5f && _door2.transform.position.x<-7.96f-5f)
+        // {
+        //     _door1.transform.position += new Vector3(0.5f,0f,0f);
+        //     _door2.transform.position -= new Vector3(0.5f,0f,0f);
+        // }
+        //
+        // // _door1.SetActive(_doorOpen);
+        // // _door2.SetActive(_doorOpen);
+        // _doorOpen = !_doorOpen;
+        _startOpen = !_startOpen;
+    }
+
+    public void Update()
+    {
+        if (!_doorOpen && _startOpen)
+        {
+            
+        }
+        
+        
     }
 
     public string GetDesciption()
