790b718b7e8ab1faebff76f750e098caf63d891f
Xdoxgg <145697777+Xdoxgg@users.noreply.github.com>
2024-11-17 23:39:36 +0300

    update UsefulItem.cs

--- Scp-Grand-Escape/Assets/Items/UsefulItem.cs
+++ Scp-Grand-Escape/Assets/Items/UsefulItem.cs
@@ -49,7 +49,7 @@ public class UsefulItem : MonoBehaviour, IInteracteble
         _playerInventary.AddItem(this);
      
         _object.transform.parent = _playerObject.transform;
-        _object.transform.rotation = _name!="Flashlight"?_playerObject.transform.rotation:_playerObject.transform.rotation;
+        _object.transform.rotation = _playerObject.transform.rotation;
         _object.transform.position = _playerObject.transform.position;
        
     }
