2c9723da8edc4cf190f9ae09c5dcbb62cec558d3
Xdoxgg <145697777+Xdoxgg@users.noreply.github.com>
2024-11-28 10:55:24 +0300

    fix scp173

--- Scp-Grand-Escape/Assets/NPC/SCP/SCP-173/SCP173Control.cs
+++ Scp-Grand-Escape/Assets/NPC/SCP/SCP-173/SCP173Control.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using System.Timers;
 
 public class SCP173Control : MonoBehaviour
 {
@@ -10,13 +11,16 @@ public class SCP173Control : MonoBehaviour
     [SerializeField] private GameObject _player;
     [SerializeField] private NavMeshAgent _navMeshAgent;
 
+
     void Start()
     {
-       // _navMeshAgent = GetComponent<NavMeshAgent>();
+        _navMeshAgent.autoBraking = false;
     }
-
+    
     void Update()
     {
+     
+        
         if (_navMeshAgent.isActiveAndEnabled)
         {
             Vector3 viewportPoint = _playerCamera.WorldToViewportPoint(_scp173.transform.position);
@@ -24,8 +28,9 @@ public class SCP173Control : MonoBehaviour
             if (viewportPoint.x >= 0 && viewportPoint.x <= 1 && viewportPoint.y >= 0 && viewportPoint.y <= 1 &&
                 viewportPoint.z > 0)
             {
-                //Debug.Log("Объект в поле зрения камеры!");
                 _navMeshAgent.isStopped = true;
+                _navMeshAgent.velocity=Vector3.zero;
+                
             }
             else
             {
