74a1788dd73c8adcd82b286bd7fc8a78ee09c40d
Xdoxgg <145697777+Xdoxgg@users.noreply.github.com>
2024-11-28 22:15:55 +0300

    fix commit

---
+++ Scp-Grand-Escape/Assets/Locations/ActiveObjects/Trigers/DoorUnlockTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DoorUnlockTrigger : MonoBehaviour
+{
+    [SerializeField] private DoorController _door;
+    [SerializeField] private DoorController _doorTrigger;
+    
+    void Update()
+    {
+        if (_doorTrigger.DoorOpen)
+        {
+            _door.Locked = false;
+           
+        }
+    }
+    
+}
