09939818ab8b0d629a76fb25210aeeb0b58c65fb
Xdoxgg <145697777+Xdoxgg@users.noreply.github.com>
2024-11-29 20:51:27 +0300

    add lock tecsture for door

--- Scp-Grand-Escape/Assets/Items/KeyCardInveractive.cs
+++ Scp-Grand-Escape/Assets/Items/KeyCardInveractive.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class KeyCardInveractive : UsefulItem
 {
     [SerializeField] private int _accept;
+    [SerializeField] private GameObject[] _keyCards;
     public KeyCardInveractive()
     {
         this.Name = "KeyCard";
@@ -15,6 +16,11 @@ public class KeyCardInveractive : UsefulItem
     {
         base.UpdateLavel();
         _accept = Lavel;
+        foreach (GameObject keyCard in _keyCards)
+        {
+            keyCard.SetActive(false);
+        }
+        _keyCards[_accept-1].SetActive(true);
     }
 
     public int Accept
