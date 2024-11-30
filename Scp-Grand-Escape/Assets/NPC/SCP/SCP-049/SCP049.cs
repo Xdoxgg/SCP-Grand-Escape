5a71fba5d691c6c448bbb6665302368ef3c30c6b
Xdoxgg <145697777+Xdoxgg@users.noreply.github.com>
2024-11-30 19:36:38 +0300

    add interier

--- Scp-Grand-Escape/Assets/NPC/SCP/SCP-049/SCP049.cs
+++ Scp-Grand-Escape/Assets/NPC/SCP/SCP-049/SCP049.cs
@@ -1,8 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SCP049 : MonoBehaviour
 {
-    [SerializeField] private Animator _animator;
+    [SerializeField] private DoorTrigger _trigger;
+    [SerializeField] private NavMeshAgent _agent;
+    
+    void Update()
+    {
+        if (!_trigger.Ready)
+        {
+            _agent.isStopped = true;
+        }
+        else
+        {
+            _agent.isStopped = false;
+        }
+    }
 }
